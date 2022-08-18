64c9a1646736528b1844ff5d15b3b52aafe2e213
tarun2002 <61038677+tarun2002@users.noreply.github.com>
2022-08-18 06:23:45 +0000

    Changed the Input taking as if no input is given it gives an alert to enter the task

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ btn.addEventListener('click',function() {
     var value = input.value;
     var text = document.createElement("p");
     text.innerText = value;
+    if (!value){
+        window.alert("Enter Your Task")
+        return 
+    }
     container.appendChild(text);
+    input.value = ""
 
     var delBtn = document.createElement("button");
     delBtn.innerText = "delete";
     container.append(delBtn);
 
+
     var compBtn = document.createElement("button");
     compBtn.innerText = "done";
     container.appendChild(compBtn);
+
     compBtn.addEventListener('click',function() {
         text.style.textDecoration = "line-through";
     })
